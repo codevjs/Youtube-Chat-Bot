c05347a174d382f9d0135596266531bad7005fd1
neutron <codevjs@gmail.com>
2021-02-23 00:13:16 +0800

    Dynamic style

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,16 +1,73 @@
 import {useEffect, useState} from "react";
-import {List, Avatar, Row, Col, Button} from 'antd';
+import {List, Avatar, Row, Col, Button, Drawer, Input, Form} from 'antd';
 import { SwitchTransition, CSSTransition } from "react-transition-group";
-import {PushpinOutlined} from "@ant-design/icons";
 import { io } from "socket.io-client";
 import "./styles.css";
+
+const pinDefaultStyle = {
+    photo : {
+        background : "#1890ff"
+    },
+    name  : {
+        background : "#1890ff",
+        color      : "#fff"
+    },
+    message : {
+        background : "#fff",
+        color      : "rgba(0, 0, 0, 0.45)"
+    }
+}
+
+if (!localStorage.getItem("pinDefaultStyle"))
+    localStorage.setItem("pinDefaultStyle", JSON.stringify(pinDefaultStyle));
 
 function App() {
 
     const [isLoading, setLoading]       = useState(true);
     const [isBannerShow, setBannerShow] = useState(true);
+    const [isDrawerShow, setDrawerShow] = useState(false);
     const [data, setData]               = useState([]);
     const [selected, setSelected]       = useState([]);
+    const [pinStyle, setPinStyle]       = useState(JSON.parse(localStorage.getItem("pinDefaultStyle")));
+    const [form]                        = Form.useForm();
+
+    const saveStyleSetting = (values) => {
+
+        const styles =  {
+            photo : {
+                background : values.avatarBackground
+            },
+            name  : {
+                background : values.nameBackground,
+                color      : values.nameColor
+            },
+            message : {
+                background : values.messageBackground,
+                color      : values.messageColor
+            }
+        }
+
+        setPinStyle(styles);
+
+        localStorage.setItem("pinDefaultStyle", JSON.stringify(styles));
+    }
+
+    const resetStyle = () => {
+
+        setPinStyle(pinDefaultStyle);
+
+        localStorage.setItem("pinDefaultStyle", JSON.stringify(pinDefaultStyle));
+
+        setTimeout(() => {
+            form.setFieldsValue({
+                avatarBackground  : pinStyle.photo.background,
+                nameBackground    : pinStyle.name.background,
+                nameColor         : pinStyle.name.color,
+                messageBackground : pinStyle.message.background,
+                messageColor      : pinStyle.message.color,
+            });
+        }, 1000)
+    }
 
     useEffect(() => {
 
@@ -57,28 +114,59 @@ function App() {
                                                         <div className={"list-selected-container"}>
                                                             <div
                                                                 className={"list list-selected"}
-                                                                style={{
-                                                                    padding : "20px",
-                                                                    background: "white",
-                                                                    borderBottom : "1px solid #ddd"
-                                                                }}
+                                                                style={{padding : "10px 22%"}}
                                                             >
-
-                                                                    <List
-                                                                        loading={isLoading}
-                                                                        itemLayout="horizontal"
-                                                                        dataSource={selected}
-                                                                        renderItem={item => (
-                                                                            <List.Item>
-                                                                                <List.Item.Meta
-                                                                                    avatar={<Avatar size={"large"} src={item.photo} />}
-                                                                                    title={<>{item.authorName} <PushpinOutlined style={{color : "#1890ff"}} /></>}
-                                                                                    description={item.message}
-                                                                                />
-                                                                                <div>{item.timestamp}</div>
-                                                                            </List.Item>
-                                                                        )}
-                                                                    />
+                                                                <List
+                                                                    style={{borderBottom : "none"}}
+                                                                    loading={isLoading}
+                                                                    itemLayout="horizontal"
+                                                                    dataSource={selected}
+                                                                    renderItem={item => (
+                                                                        <List.Item>
+                                                                            <List.Item.Meta
+                                                                                avatar={
+                                                                                    <Avatar
+                                                                                        style={{
+                                                                                            border : `3px solid ${pinStyle.photo.background}`,
+                                                                                            borderRadius : "50%"
+                                                                                        }}
+                                                                                        size={50}
+                                                                                        src={item.photo}
+                                                                                    />
+                                                                                }
+                                                                                title={
+                                                                                    <div style={{
+                                                                                        background : pinStyle.name.background,
+                                                                                        color : pinStyle.name.color,
+                                                                                        marginTop : 10,
+                                                                                        padding : "5px 0 5px 40px",
+                                                                                        marginLeft : "-45px",
+                                                                                        width : 500,
+                                                                                        borderRadius : 5
+                                                                                    }}>
+                                                                                        {item.authorName}
+                                                                                    </div>
+                                                                                }
+                                                                                description={
+                                                                                    <div
+                                                                                        style={{
+                                                                                            background : pinStyle.message.background,
+                                                                                            color : pinStyle.message.color,
+                                                                                            marginTop : "-5px",
+                                                                                            marginLeft : "-45px",
+                                                                                            padding : "5px 0 5px 40px",
+                                                                                            paddingTop : 10,
+                                                                                            width : 550,
+                                                                                            borderRadius : 5
+                                                                                        }}
+                                                                                    >
+                                                                                        {item.message}
+                                                                                    </div>
+                                                                                }
+                                                                            />
+                                                                        </List.Item>
+                                                                    )}
+                                                                />
                                                             </div>
                                                         </div>
                                                     </CSSTransition>
@@ -101,7 +189,8 @@ function App() {
                             padding      : 20,
                             background   : "white",
                             height       : "62.5vh",
-                            overflow     : "auto"
+                            overflow     : "auto",
+                            borderTopLeftRadius : 5
                         }}
                     >
                         <List
@@ -133,7 +222,8 @@ function App() {
                             padding      : 20,
                             background   : "white",
                             height       : "62.5vh",
-                            overflow     : "auto"
+                            overflow     : "auto",
+                            borderTopRightRadius : 5
                         }}
                     >
                         <div style={{marginBottom : 20}}>
@@ -143,12 +233,13 @@ function App() {
                                 alt={""}
                             />
                         </div>
-                        <Row gutter={[10, 30]}>
+                        <Row gutter={[10, 10]}>
                             <Col span={12}>
                                 <Button
-                                    style={{width : "100%"}}
+                                    style={{width : "100%", borderRadius : 5}}
                                     size={"large"}
                                     // type={"primary"}
+                                    // ghost={true}
                                     onClick={() => setBannerShow(true)}
                                 >
                                     Show Banner
@@ -156,7 +247,7 @@ function App() {
                             </Col>
                             <Col span={12}>
                                 <Button
-                                    style={{width : "100%"}}
+                                    style={{width : "100%", borderRadius : 5}}
                                     size={"large"}
                                     // type={"primary"}
                                     // ghost={true}
@@ -165,12 +256,12 @@ function App() {
                                     Hide Banner
                                 </Button>
                             </Col>
-                            <Col span={24}>
+                            <Col span={12}>
                                 <Button
-                                    style={{width : "100%"}}
+                                    style={{width : "100%", borderRadius : 5}}
                                     size={"large"}
-                                    type={"primary"}
-                                    danger={true}
+                                    // ghost={true}
+                                    // danger={true}
                                     onClick={() => {
                                         setBannerShow(true);
                                         setSelected([]);
@@ -179,10 +270,88 @@ function App() {
                                     Clear Pinned
                                 </Button>
                             </Col>
+                            <Col span={12}>
+                                <Button
+                                    style={{width : "100%", borderRadius : 5}}
+                                    size={"large"}
+                                    onClick={() => setDrawerShow(true)}
+                                >
+                                    Style Setting
+                                </Button>
+                            </Col>
                         </Row>
                     </div>
                 </Col>
             </Row>
+
+            <Drawer
+                title={null}
+                placement={"right"}
+                visible={isDrawerShow}
+                closable={false}
+                onClose={() => setDrawerShow(false)}
+                width={350}
+            >
+                {/*<h4>Pinned Style</h4>*/}
+                <div style={{position :"relative", height : "100%"}}>
+                    <Form
+                        layout={"vertical"}
+                        onFinish={saveStyleSetting}
+                        form={form}
+                        initialValues={{
+                            avatarBackground  : pinStyle.photo.background,
+                            nameBackground    : pinStyle.name.background,
+                            nameColor         : pinStyle.name.color,
+                            messageBackground : pinStyle.message.background,
+                            messageColor      : pinStyle.message.color,
+                        }}
+                    >
+                        <Form.Item label={"Avatar Background"} name={"avatarBackground"}>
+                            <Input size={"large"} />
+                        </Form.Item>
+                        <Form.Item label={"Name Background"} name={"nameBackground"}>
+                            <Input size={"large"} />
+                        </Form.Item>
+                        <Form.Item label={"Name Text Color"} name={"nameColor"}>
+                            <Input size={"large"} />
+                        </Form.Item>
+                        <Form.Item label={"Message Background"} name={"messageBackground"}>
+                            <Input size={"large"} />
+                        </Form.Item>
+                        <Form.Item label={"Message Text Color"} name={"messageColor"}>
+                            <Input size={"large"} />
+                        </Form.Item>
+                    </Form>
+
+                    <div style={{
+                        position : "absolute",
+                        width : "100%",
+                        bottom : 0
+                    }}>
+                        <Row gutter={[5,5]}>
+                            <Col span={12}>
+                                <Button
+                                    size={"large"}
+                                    style={{width : "100%"}}
+                                    onClick={resetStyle}
+                                >
+                                    Reset
+                                </Button>
+                            </Col>
+                            <Col span={12}>
+                                <Button
+                                    type={"submit"}
+                                    size={"large"}
+                                    style={{width : "100%"}}
+                                    onClick={() => form.submit()}
+                                >
+                                    Save
+                                </Button>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </Drawer>
         </div>
     );
 }
